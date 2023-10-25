a8d16742f3d2425c07a23649e69be4b3508b049d
marcosgontijo <marcoseduardogontijo@gmail.com>
2023-10-25 14:49:01 -0300

    Alteracoes no controller para utilizar o copyNonNullProperties criado em uma classe no Utils

--- todolist/src/main/java/br/com/marcosgontijo/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/marcosgontijo/todolist/task/TaskController.java
@@ -9,11 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.marcosgontijo.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest; 
 
@@ -48,5 +51,18 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    //http://localhost:8080/tasks/09808098-asdasdasd-121212
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        
+        return this.taskRepository.save(task);
     }
 }
